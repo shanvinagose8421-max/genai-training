f37f8bf48adf2a554b5076ce7b3413f4cfcc3ea9
shanvinagose8421-max <shanvinagose8421@gmail.com>
2026-07-27 17:04:14 +0530

    Add files via upload

---
+++ shanvigpt.py
@@ -0,0 +1,30 @@
+
+import os
+import streamlit as st
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import OllamaLLM 
+
+
+#prompt template
+prompt=ChatPromptTemplate.from_messages(
+    [
+        ("system","You are a helpful assistant. Please respond to the question asked"),
+        ("user","Question:{question}")
+    ]
+)
+
+# streamlit template
+st.title("shanvi chatbot")
+input_text=st.text_input("What question you have in mind?")
+
+# ollama model
+
+llm = OllamaLLM(model="gemma2:2b")
+
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+st.write(chain.invoke({"question":input_text}))
+
+
